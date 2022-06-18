ec8c5b65d5c93d6e28dc68cbe8524366969dd919
Layanne Moraes <104206895+Anne-Moraes@users.noreply.github.com>
2022-06-18 14:54:48 -0300

    Atualização

--- Semana1Dia1Ex1/Program.cs
+++ Semana1Dia1Ex1/Program.cs
@@ -16,27 +16,23 @@ namespace Semana2Dia1Ex1
             string nomeDoAluno = Console.ReadLine();
 
             Console.WriteLine("Qual o número da matrícula?");
-            aluno.Matricula = int.Parse(Console.ReadLine());
+            aluno.Matricula = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Qual a nota da primeira prova do aluno?");
-            aluno.Nota1 = int.Parse(Console.ReadLine());
+            aluno.Nota1 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Qual a nota da segunda prova do aluno?");
-            aluno.Nota2 = int.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Qual a nota de trabalho do aluno?");
-            aluno.Trabalho = int.Parse(Console.ReadLine());
+            aluno.Trabalho = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($" A matricula do aluno é: {aluno.Matricula} \nA nota da primeira prova do aluno é: {aluno.Nota1} \nA nota da segunda prova do aluno é: {aluno.Nota2} \nA nota de trabalho do aluno é: {aluno.Trabalho}");
+            Console.WriteLine($" A matricula do aluno é: {aluno.Matricula.ToString("F2")} \nA nota da primeira prova do aluno é: {aluno.Nota1.ToString("F2")} \nA nota da segunda prova do aluno é: {aluno.Nota2.ToString("F2")} \nA nota de trabalho do aluno é: {aluno.Trabalho.ToString("F2")}");
 
-            Console.WriteLine($"A Media final do aluno é {aluno.MediaFinal()}");
+            Console.WriteLine($"A Media final do aluno é {aluno.MediaFinal().ToString("F2")}");
 
-            Console.WriteLine($"A nota na prova final precisa ser no minimo {aluno.ProvaFinal()}");
-         
-
-            
-
-
+            Console.WriteLine($"A nota na prova final para o resutado máximo é: {aluno.ProvaFinal().ToString("F2")}");
+ 
         }
     }
 }
