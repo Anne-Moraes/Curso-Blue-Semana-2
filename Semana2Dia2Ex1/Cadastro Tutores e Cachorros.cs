183a4e1a32aff9e5c3076f955762c8e40116e497
Layanne Moraes <104206895+Anne-Moraes@users.noreply.github.com>
2022-06-20 19:51:22 -0300

    Atualização

---
+++ Semana2Dia2Ex1/Cadastro Tutores e Cachorros.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Semana2Dia1Ex2 
+{
+    internal class Enunciado
+    {  
+        // dois construtores, atributos privados, get set, private, herança 
+        /*
+        Vamos modelar um cadastro de animais de uma pet-shop? 
+        Crie uma classe que modele os animais e contenha como atributos nome, espécie, cor, idade, observação e dados do dono. 
+        Os atributos relacionados ao dono do animal deve ser privados para proteção de dados. 
+        Forrneça métodos para cadastro e leitura das informações.
+        */      
+
+        //Metodos    
+    }
+    public class Pet
+    { 
+        //Atributos Públicos
+        public string NomePet;
+        public string Especie;
+        public string Raça;
+        public int Idade;
+        public string Cor;
+        public string Porte;
+        public string Tutor;
+        public string Observacao;
+
+        public Pet(string nomePet, string especie, string raça, int idade, string cor, string porte, string tutor, string observacao)
+
+        {
+            NomePet = nomePet;
+            Especie = especie;
+            Raça = raça;
+            Idade = idade;
+            Cor = cor;
+            Porte = porte;
+            Tutor = tutor;
+            Observacao = observacao;           
+        }
+        public void Cadastrar()
+        {        
+            Console.WriteLine($"Os dados do seu Pet são \nNome: {NomePet} \nEspecie: {Especie} \nRaça: {Raça} \nIdade: {Idade} \nCor: {Cor} \nPorte: {Porte} \nTutor {Tutor} \nObservações: {Observacao}");
+        }
+    }
+    public class Tutor
+    {
+        //Atributos Privados
+        private string Nome;           
+        private int Telefone;
+        private int Documento;
+        private string DataDeNascimento;
+
+        public Tutor(string nome, int telefone, int documento, string dataDeNascimento)
+        {
+            Nome = nome;               
+            Telefone = telefone;
+            Documento = documento;
+            DataDeNascimento = dataDeNascimento;
+
+        }
+        public void CadastrarTutor()
+        {
+            Console.WriteLine("Digite as seguintes informações sobre você:");
+
+            Console.WriteLine("Nome");
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("Especie");
+            Telefone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Raça");
+            Documento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Data de nascimento");
+            DataDeNascimento = Console.ReadLine();
+        }
+    }
+}
--- Semana2Dia2Ex1/Program.cs
+++ Semana2Dia2Ex1/Program.cs
@@ -1,22 +1,74 @@
-﻿namespace Semana2Dia1Ex2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2Dia1Ex2
 {
     internal class Program
     {
         static void Main()
         {
             //Como eu faço para o úsuario passar os parametros?
-            Console.WriteLine("Digite o nome, o cargo e o salário do empregado (nessa ordem):");
 
-            Empregado empregado1 = new Empregado(Console.ReadLine(), Console.ReadLine(), double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o Nome do pet:");
+            string nomePet = Console.ReadLine();
 
-            //empregado1.Aumento();
-            //Console.WriteLine(empregado1.Aumento());
+            Console.WriteLine("Digite a Especie do pet:");
+            string especie = Console.ReadLine();
 
-            empregado1.Imprimir();
+            Console.WriteLine("Digite a Raça do pet:");
+            string raca = Console.ReadLine();
 
-            empregado1.SalarioNegativo();
+            Console.WriteLine("Digite a Idade do pet:");
+            int idade = int.Parse(Console.ReadLine());
 
-            empregado1.SalarioNegativo();
+            Console.WriteLine("Digite a cor do pet:");
+            string cor = Console.ReadLine();
+
+            Console.WriteLine("Digite o Porte do pet:");
+            string porte = Console.ReadLine();
+
+            Console.WriteLine("Digite o Nome do Tutor do Pet");
+            string tutor = Console.ReadLine();
+
+            Console.WriteLine("Alguma observação?");
+            string observacao = Console.ReadLine();
+
+            string nomepet = Console.ReadLine();
+            Pet pet1 = new Pet(nomePet, especie, raca, idade, cor, porte, tutor, observacao);
+            
+            List<Pet> cadastro = new List<Pet>();
+
+            pet1.Cadastrar();
+
+            Console.WriteLine("Digite o Nome do Tutor:");
+            string nomeTutor = Console.ReadLine();
+
+            Console.WriteLine("Digite o Telefone  do Tutor:");
+            int telefone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Documento  do Tutor:");
+            int documento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Data de Nascimento do Tutor:");
+            string dataNascimento = Console.ReadLine();
+
+            Tutor tutor1 = new Tutor(nomeTutor, telefone, documento, dataNascimento);
+
+            tutor1.CadastrarTutor();
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
